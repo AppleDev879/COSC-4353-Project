f346846d3e8f5d947d4bcea271c266da698a1700
ljmeza <lizandrom6@gmail.com>
2022-04-25 11:24:28 -0500

    update

--- OilApp/server/index.js
+++ OilApp/server/index.js
@@ -208,12 +208,12 @@ app.post("/api/profile",(req,res)=>
     const zip =req.body.zip;
     const st=req.body.ST;
     const userID=req.body.userID;
-    const sqlInsert="UPDATE users (name, address1, address2, city,zip, state) VALUES(?,?,?,?,?,?)"
-    +"WHERE ID ="+ userID;
-    connection.query(sqlInsert,[ Name, address1, address2, city,zip, st],(err,res)=>{
+    const sqlInsert="UPDATE users SET?????? WHERE ID ="+ userID;
+    connection.query(sqlInsert,[{} name:Name, address1:address1, address2:address2, city:city,zip:zip, state;st}],(err,res)=>{
         console.log(err);
     } );
-});
+}); 
+
 
 
 
